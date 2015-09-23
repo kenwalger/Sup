4e8cd39d8cc69ae1ee82c22fb5872acbb126a4a1
Ken W. Alger <kenalger@comcast.net>
2015-09-23 14:52:14 -0700

    Course extension: environment variables for Twitter keys

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -11,7 +11,7 @@ public class Controller {
 
     public void handleSaySup(ActionEvent actionEvent) {
         System.out.printf("Sup %s! %n", twitterHandle.getText());
-        String tweet = "@" + twitterHandle.getText() + " Sup? \n \n -- from the Sup? App";
+        String tweet = "@" + twitterHandle.getText() + " Sup? \n \n -- from the JavaFX Sup? App \n by Treehouse";
         SupTweet.postTweet(tweet);
     }
 }
